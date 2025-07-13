dfc8ca3d9fc0b482ad01eaa3f906cceaf81552f2
Abdi <sonypsn2569@gmail.com>
2025-07-13 22:06:16 +0200

    Bluetooth communication between bluetooth terminal on mobile device and the serial monitor on the pc has been tested and works as it should

--- Smart_door_chip/Smart_door_chip.ino
+++ Smart_door_chip/Smart_door_chip.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+SoftwareSerial btSerial(D7, D6); // RX = D7, TX = D6
+
+void setup() {
+  Serial.begin(115200);        // PC Serial Monitor
+  delay(5000);               // A little delay so that serial monitor can open
+
+  btSerial.begin(9600);      // HC-05 default baud
+  
+
+  Serial.println(">>> Starting test with SofwareSerial on D7 (GPIO13)");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // From mobile device to PC 
+ if (btSerial.available()) {
+    delay(100); // Delay to make sure the whole message gets recieved in one line
+    Serial.print("Fra HC-05: ");
+    while (btSerial.available()) {
+      char c = btSerial.read();
+      Serial.print(c);
+    }
+    Serial.println();
+  }
 
+  // From PC to mobile device 
+  if (Serial.available()){
+    Serial.print("Fra serial monitor: ");
+  
+  while (Serial.available()) {
+    char c = Serial.read();
+    Serial.print(c); 
+    btSerial.print(c);
+    }
+  }
 }
